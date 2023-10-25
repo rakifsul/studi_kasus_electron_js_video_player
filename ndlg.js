a578a454400edabed35178e3de6a3e86815fa432
shbfrlnc <allwebs200@gmail.com>
2023-10-26 03:28:52 +0700

    GANTI LISENSI KE AGPL

--- ndlg.js
+++ ndlg.js
@@ -4,7 +4,7 @@
 // begin: import modules.
 const fs = require("fs");
 const path = require("path");
-const checker = require('license-checker');
+const checker = require("license-checker");
 // end: import modules.
 
 // folder tempat meletakkan license 3rd party.
@@ -18,24 +18,27 @@ if (!fs.existsSync(licenseFolder)) {
 // jalankan license-checker.
 // dimulai dari folder di mana script ini berada.
 // yakni, dalam hal ini, root folder.
-checker.init({
-    start: './',
-    deps: 'true'
-}, function (err, packages) {
-    if (err) {
-    } else {
-        console.log(packages);
-        let theParsed = packages;
+checker.init(
+    {
+        start: "./",
+        deps: "true",
+    },
+    function (err, packages) {
+        if (err) {
+        } else {
+            console.log(packages);
+            let theParsed = packages;
 
-        // untuk setiap file license yang di-parse,
-        // jika file license tersebut ada,
-        // copy ke target folder.
-        for (let obj in theParsed) {
-            if (fs.existsSync(theParsed[obj].licenseFile)) {
-                console.log(theParsed[obj].licenseFile);
-                let tgt = licenseFolder + path.sep + path.dirname(theParsed[obj].licenseFile).split(path.sep).pop() + ".txt";
-                fs.copyFileSync(theParsed[obj].licenseFile, tgt);
+            // untuk setiap file license yang di-parse,
+            // jika file license tersebut ada,
+            // copy ke target folder.
+            for (let obj in theParsed) {
+                if (fs.existsSync(theParsed[obj].licenseFile)) {
+                    console.log(theParsed[obj].licenseFile);
+                    let tgt = licenseFolder + path.sep + path.dirname(theParsed[obj].licenseFile).split(path.sep).pop() + ".txt";
+                    fs.copyFileSync(theParsed[obj].licenseFile, tgt);
+                }
             }
         }
     }
-});
+);
